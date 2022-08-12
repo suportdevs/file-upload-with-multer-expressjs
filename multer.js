f46ab7bdc6a62de31b1f0b5526030c2c8b515c6e
suportdevs <suportdevs@gmail.com>
2022-08-13 00:32:35 +0600

    understanding multer expressjs and uploads files

--- multer.js
+++ multer.js
@@ -1,14 +1,63 @@
 const express = require('express');
 const multer = require('multer');
+const path = require('path');
 
 const app = express();
-const Upload_Folder = './uploads/';
+const UPLOAD_FOLDER = './uploads/';
 
-const upload = multer({dest: Upload_Folder});
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, UPLOAD_FOLDER)
+    },
+    filename: (req, file, cb) => {
+        const fileExt = path.extname(file.originalname);
+        const fileName = file.originalname.replace(fileExt, "").split(" ").join("-") + "-" + Date.now();
+        cb(null, fileName + fileExt);
+    }
+})
 
-app.post('/upload', upload.single('avatar', 3), (req, res, next) => {
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1000000 // 1MB
+    },
+    fileFilter: (req, file, cb) => {
+        if(file.fieldname == 'avatar'){
+            if(file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg'){
+                cb(null, true)
+            } else {
+                cb(new Error("Only .png .jpg or .jpeg file allowed!"))
+            }
+        } else if(file.fieldname == 'json'){
+            if(file.mimetype == 'application/json'){
+                cb(null, true)
+            } else {
+                cb(new Error("Only .json file allowed!"))
+            }
+        } else {
+            cb(new Error("There was an unknown error!"))
+        }
+    }
+});
+
+app.post('/upload', upload.fields([
+    {name: 'avatar', maxCount: 2},
+    {name: 'json', maxCount: 2}
+]), (req, res, next) => {
     console.log(req.body);
     res.send("File uploaded successfull.");
+})
+
+app.use((err, req, res, next) => {
+    if(err){
+        if(err instanceof multer.MulterError){
+            res.status(500).send("There was an upload error")
+        } else {
+            res.send(err.message);
+        }
+    } else {
+        res.send('success');
+    }
 })
 
 app.listen(3000, () => {
